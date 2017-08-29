1ea7aa1dbdaabf65b74d1a4723fb0c3daf73fe9c
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2017-08-29 17:04:08 -0500

    :computer: setting the PDO attribute PDO::ATTR_EMULATE_PREPARES to true

--- config/database.php
+++ config/database.php
@@ -64,6 +64,8 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
+            'options' => [PDO::ATTR_EMULATE_PREPARES => true]
+
         ],
 
         'pgsql' => [
